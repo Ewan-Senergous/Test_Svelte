a9aa8724a405bd1e88968ca67824c5552aa0986b
Ewan Senergous <ewan16270409@outlook.fr>
2024-12-26 11:38:14 +0100

    ✨ Add emoji error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { emojis } from './emojis.js';
+	import { get } from 'svelte/store';
+
+	const status = get(page).status || 500;
+	const errorMessage = get(page).error?.message || 'Une erreur inconnue est survenue.';
+</script>
+
+<h1>{status} {errorMessage}</h1>
+
+<span class="center-emoji">
+	{emojis[status as keyof typeof emojis] ?? emojis[500]}
+</span>
+
+<style>
+	.center-emoji {
+		font-size: 5rem;
+		display: block;
+		text-align: center;
+	}
+</style>
